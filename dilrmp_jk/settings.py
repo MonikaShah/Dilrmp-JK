4b70e3feb4dd57699bff6fdaea8f54c5af22dc1f
monikashah <monikapatira@gmail.com>
2025-04-27 22:13:22 +0530

    settings changed

--- dilrmp_jk/settings.py
+++ dilrmp_jk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w=9of!2r)o!dksukq9ijoj_*td8@7dk3vrw!^zkj8@q)-ub$ih
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['youth.communitygis.in','localhost']
+ALLOWED_HOSTS = ['youth.communitygis.in','65.109.14.195']
 
 
 # Application definition
